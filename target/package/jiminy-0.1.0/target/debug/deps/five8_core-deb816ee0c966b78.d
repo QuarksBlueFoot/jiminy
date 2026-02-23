fdca79176bebd0dcbfefca7ceac2867a67a4bf3a
QuarksBlueFoot <quark@bluefoot.tech>
2026-02-23 09:52:49 -0600

    chore: add .gitignore, remove committed target dir

--- target/package/jiminy-0.1.0/target/debug/deps/five8_core-deb816ee0c966b78.d
+++
@@ -1,8 +0,0 @@
-D:\tmp\jiminy-standalone\target\package/jiminy-0.1.0\target\debug\deps\five8_core-deb816ee0c966b78.d: C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\five8_core-0.1.2\src\lib.rs C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\five8_core-0.1.2\src\../README.md
-
-D:\tmp\jiminy-standalone\target\package/jiminy-0.1.0\target\debug\deps\libfive8_core-deb816ee0c966b78.rlib: C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\five8_core-0.1.2\src\lib.rs C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\five8_core-0.1.2\src\../README.md
-
-D:\tmp\jiminy-standalone\target\package/jiminy-0.1.0\target\debug\deps\libfive8_core-deb816ee0c966b78.rmeta: C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\five8_core-0.1.2\src\lib.rs C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\five8_core-0.1.2\src\../README.md
-
-C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\five8_core-0.1.2\src\lib.rs:
-C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\five8_core-0.1.2\src\../README.md:
--- target/package/jiminy-0.1.0/target/debug/deps/solana_program_error-4e1041035f968d69.d
+++
@@ -1,7 +0,0 @@
-D:\tmp\jiminy-standalone\target\package/jiminy-0.1.0\target\debug\deps\solana_program_error-4e1041035f968d69.d: C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\solana-program-error-3.0.0\src\lib.rs
-
-D:\tmp\jiminy-standalone\target\package/jiminy-0.1.0\target\debug\deps\libsolana_program_error-4e1041035f968d69.rlib: C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\solana-program-error-3.0.0\src\lib.rs
-
-D:\tmp\jiminy-standalone\target\package/jiminy-0.1.0\target\debug\deps\libsolana_program_error-4e1041035f968d69.rmeta: C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\solana-program-error-3.0.0\src\lib.rs
-
-C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\solana-program-error-3.0.0\src\lib.rs:
