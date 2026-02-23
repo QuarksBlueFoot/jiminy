5a88545e9a167f338c99e5def7eeae14dbb73aeb
QuarksBlueFoot <quark@bluefoot.tech>
2026-02-23 09:52:49 -0600

    chore: add .gitignore, remove committed target dir

--- target/package/jiminy-0.1.0/target/debug/deps/pinocchio_pubkey-9b12788841de8b42.d
+++
@@ -1,7 +0,0 @@
-D:\tmp\jiminy-standalone\target\package/jiminy-0.1.0\target\debug\deps\pinocchio_pubkey-9b12788841de8b42.d: C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pinocchio-pubkey-0.3.0\src\lib.rs
-
-D:\tmp\jiminy-standalone\target\package/jiminy-0.1.0\target\debug\deps\libpinocchio_pubkey-9b12788841de8b42.rlib: C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pinocchio-pubkey-0.3.0\src\lib.rs
-
-D:\tmp\jiminy-standalone\target\package/jiminy-0.1.0\target\debug\deps\libpinocchio_pubkey-9b12788841de8b42.rmeta: C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pinocchio-pubkey-0.3.0\src\lib.rs
-
-C:\Users\matts\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pinocchio-pubkey-0.3.0\src\lib.rs:
